e4350940de825220d75151bdb1f0c275433b5cb7
HameedMulani <hameedmulani2001@gmail.com>
2024-09-06 20:21:00 +0530

    All End Points of API Added

---
+++ SI_Sponsorship-main/CodeMarathon_sponsorship/Dao/IPaymentDao.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using CodeMarathon_sponsorship.Models;
+
+namespace CodeMarathon_sponsorship.Dao
+{
+    public interface IPaymentDao
+    {
+
+        Task<int> InsertPayment(Payment p);
+        Task<List<Payment>> GetPayments();
+        Task<List<SponsorPaymentSummary>> GetSponsorPaymentSummaries();
+
+        Task<List<MatchDetails>> GetMatchDetails();
+
+        Task<List<SponsorMatchCount>> GetSponsorsAndMatchCounts(int year);
+
+        Task<int> UpdatePaymentsById(int id, double newprice);
+        Task<int> DeletePaymentById(int id);
+    }
+}
